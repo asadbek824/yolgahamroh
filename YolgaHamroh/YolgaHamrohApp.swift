79fbd0f29aa8b3cc4a209212cd393663bbbba8ca
Asadbek Yoldoshev <asadbek.yu@payme.uz>
2025-08-01 14:26:09 +0500

    Initial Commit

---
+++ YolgaHamroh/YolgaHamrohApp.swift
@@ -0,0 +1,17 @@
+//
+//  YolgaHamrohApp.swift
+//  YolgaHamroh
+//
+//  Created by Yo'ldoshev Asadbek on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct YolgaHamrohApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
